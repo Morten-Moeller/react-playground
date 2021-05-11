83edd82acc61ab0f505505926bf9f7bc6c367030
Morten Möller <git@mortenm.de>
2021-05-11 16:18:16 +0200

    Add data and renderCards

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,26 @@ import * as React from 'react'
 import Button from './Button'
 import Card from './Card'
 import Header from './Header'
-import Pagination from './Pagination'
 import './App.css'
+import cards from './data.json'
+import decodeHtml from './decodeHtml'
 
 export default () => {
+  const { question, correct_answer } = cards
+  const renderCards = cards.map(
+    ({ category: heading, question, correct_answer: answer }) => (
+      <Card
+        key={question}
+        heading={heading}
+        question={decodeHtml(question)}
+        answer={decodeHtml(answer)}
+      />
+    )
+  )
   return (
     <div className="App">
       <Header headerText="Home" isHidden />
-      <Header headerText="Settings" />
-      <Header headerText="Profile" isHidden />
-      <Button isActive text="Hit me!" />
-      <Button text="Hit me hard!" />
-      <Pagination currentPage={1} totalPage={32} />
-      <Pagination currentPage={2} totalPage={32} />
-      <Pagination currentPage={32} totalPage={32} />
-
-      <Card heading="Card 1" paragraph="Lorem ipsum dolor sit." />
-      <Card heading="Card 2" paragraph="Lorem ipsum dolor sit." isBookmarked />
-      <Card heading="Card 3" paragraph="Lorem ipsum dolor sit." />
+      {renderCards}
     </div>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,19 +1,20 @@
 import * as React from 'react'
 import './Card.css'
 
-export default ({ heading, paragraph, isBookmarked }) => {
+export default ({ heading, question, answer, isBookmarked }) => {
+  const tags = ['trivia', 'q&a', 'english']
+  const renderTags = tags.map(tag => <li>{tag}</li>)
+  const bookmark = isBookmarked
+    ? 'Card__bookmark Card__bookmark--active'
+    : 'Card__bookmark'
   return (
     <section className="Card">
-      <button
-        aria-label="Bookmark card"
-        className={
-          isBookmarked
-            ? 'Card__bookmark Card__bookmark--active'
-            : 'Card__bookmark'
-        }
-      ></button>
+      <button aria-label="Bookmark card" className={bookmark}></button>
       <h2 className="Card__heading">{heading}</h2>
-      <p className="Card__paragraph">{paragraph}</p>
+      <p className="Card__question">{question}</p>
+      <button className="Card__button">Show answer</button>
+      <p className="Card__answer hidden">{answer}</p>
+      <ul className="Card__tag-list">{renderTags}</ul>
     </section>
   )
 }
