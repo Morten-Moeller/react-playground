eaafd9b2d9531659e49f3e15d612bd691268c4fb
Morten Möller <git@mortenm.de>
2021-05-11 10:49:00 +0200

    Bookmark changed to button element

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,19 @@ import './App.css'
 export default () => {
   return (
     <div className="App">
-      {/* <Header headerText="Home" isHidden />
+      <Header headerText="Home" isHidden />
       <Header headerText="Settings" />
       <Header headerText="Profile" isHidden />
       <Button isActive text="Hit me!" />
       <Button text="Hit me hard!" />
       <Pagination inActiveLeft page=" 1/32 " />
       <Pagination page=" 2/32 " />
-      <Pagination inActiveRight page=" 32/32 " /> */}
+      <Pagination inActiveRight page=" 32/32 " />
 
-      <Card heading="Title" paragraph="Lorem ipsum dolor sit." />
-      <Card heading="Title" paragraph="Lorem ipsum dolor sit." isBookmarked />
-      <Card heading="Title" paragraph="Lorem ipsum dolor sit." />
+      <Card heading="Card 1" paragraph="Lorem ipsum dolor sit." />
+      <Card heading="Card 2" paragraph="Lorem ipsum dolor sit." isBookmarked />
+      <Card heading="Card 3" paragraph="Lorem ipsum dolor sit." />
+      <Card heading="Card 4" paragraph="Lorem ipsum dolor sit." />
     </div>
   )
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -4,13 +4,14 @@ import './Card.css'
 export default ({ heading, paragraph, isBookmarked }) => {
   return (
     <section className="Card">
-      <div
+      <button
+        aria-label="Bookmark card"
         className={
           isBookmarked
             ? 'Card__bookmark Card__bookmark--active'
             : 'Card__bookmark'
         }
-      ></div>
+      ></button>
       <h2 className="Card__heading">{heading}</h2>
       <p className="Card__paragraph">{paragraph}</p>
     </section>
