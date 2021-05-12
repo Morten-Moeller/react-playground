b75e396f1042de05b06fdfe0ad632555922272ba
Morten Möller <git@mortenm.de>
2021-05-12 12:23:19 +0200

    Add button state

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import './Card.css'
 
 export default ({ heading, question, answer, isBookmarked }) => {
+  const [isHidden, setIsHidden] = React.useState(true)
   const tags = ['trivia', 'q&a', 'english']
   const bookmark = isBookmarked
     ? 'Card__bookmark Card__bookmark--active'
@@ -11,8 +12,12 @@ export default ({ heading, question, answer, isBookmarked }) => {
       <button aria-label="Bookmark card" className={bookmark}></button>
       <h2 className="Card__heading">{heading}</h2>
       <p className="Card__question">{question}</p>
-      <button className="Card__button">Show answer</button>
-      <p className="Card__answer hidden">{answer}</p>
+      <button onClick={() => setIsHidden(!isHidden)} className="Card__button">
+        Show answer
+      </button>
+      <p className={isHidden ? 'Card__answer hidden' : 'Card__answer'}>
+        {answer}
+      </p>
       <ul className="Card__tag-list">
         {tags.map(tag => (
           <li key={tag}>{tag}</li>
